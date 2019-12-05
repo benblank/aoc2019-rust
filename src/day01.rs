febb96d068b81d4364199c8c8069bea8db3a8136
Ben Blank <bblank@linkedin.com>
2019-12-05 09:40:18 -0800

    Complete day 1, part 2.

--- src/day01.rs
+++ src/day01.rs
@@ -10,12 +10,31 @@ fn calculate_fuel_cost(weight: i32) -> i32 {
   }
 }
 
+fn recursively_calculate_fuel_cost(weight: i32) -> i32 {
+  let fuel_cost = calculate_fuel_cost(weight);
+
+  if fuel_cost > 0 {
+    fuel_cost + recursively_calculate_fuel_cost(fuel_cost)
+  } else {
+    fuel_cost
+  }
+}
+
 pub fn part1() {
   let stdin = io::stdin();
   let handle = stdin.lock();
 
   println!("Total fuel requirements: {}", handle.lines().map(|line| {
     calculate_fuel_cost(line.unwrap().parse::<i32>().unwrap())
+  }).sum::<i32>());
+}
+
+pub fn part2() {
+  let stdin = io::stdin();
+  let handle = stdin.lock();
+
+  println!("Total fuel requirements: {}", handle.lines().map(|line| {
+    recursively_calculate_fuel_cost(line.unwrap().parse::<i32>().unwrap())
   }).sum::<i32>());
 }
 
@@ -29,5 +48,12 @@ mod tests {
     assert_eq!(2, calculate_fuel_cost(14));
     assert_eq!(654, calculate_fuel_cost(1969));
     assert_eq!(33583, calculate_fuel_cost(100_756));
+  }
+
+  #[test]
+  fn recursively_calculate_fuel_cost_works() {
+    assert_eq!(2, recursively_calculate_fuel_cost(14));
+    assert_eq!(966, recursively_calculate_fuel_cost(1969));
+    assert_eq!(50346, recursively_calculate_fuel_cost(100_756));
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     for arg in env::args().skip(1) {
         match arg.as_ref() {
             "1.1" => day01::part1(),
+            "1.2" => day01::part2(),
             _ => eprintln!("Day {} hasn't been written yet!", arg),
         }
     }
