e2add25ba71417e80ad25118a95c34532ecc6334
Ben Blank <ben.blank@gmail.com>
2019-12-13 15:25:41 -0800

    Complete day 11, part 2.

--- src/day11.rs
+++ src/day11.rs
@@ -1,32 +1,14 @@
 use crate::intcomp::{read_program, Intcomp};
 use std::collections::HashMap;
+use std::i32;
 
 const INPUT_PATH: &str = "day11.input.txt";
 
-fn turn_left(facing: (i32, i32)) -> (i32, i32) {
-    match facing {
-        (-1, 0) => (0, 1),
-        (0, -1) => (-1, 0),
-        (0, 1) => (1, 0),
-        (1, 0) => (0, -1),
-        _ => panic!("invalid facing {:?}", facing),
-    }
-}
+fn paint_hull(hull: &mut HashMap<(i32, i32), i64>, start_panel_color: i64) {
+    hull.insert((0, 0), start_panel_color);
 
-fn turn_right(facing: (i32, i32)) -> (i32, i32) {
-    match facing {
-        (-1, 0) => (0, -1),
-        (0, -1) => (1, 0),
-        (0, 1) => (-1, 0),
-        (1, 0) => (0, 1),
-        _ => panic!("invalid facing {:?}", facing),
-    }
-}
-
-pub fn part1() {
     let initializer = read_program(INPUT_PATH);
     let mut intcomp = Intcomp::new(&initializer);
-    let mut hull = HashMap::new();
     let mut position = (0, 0);
     let mut facing = (0, -1); // "Up".
 
@@ -58,6 +40,77 @@ pub fn part1() {
             break;
         }
     }
+}
+
+fn turn_left(facing: (i32, i32)) -> (i32, i32) {
+    match facing {
+        (-1, 0) => (0, 1),
+        (0, -1) => (-1, 0),
+        (0, 1) => (1, 0),
+        (1, 0) => (0, -1),
+        _ => panic!("invalid facing {:?}", facing),
+    }
+}
+
+fn turn_right(facing: (i32, i32)) -> (i32, i32) {
+    match facing {
+        (-1, 0) => (0, -1),
+        (0, -1) => (1, 0),
+        (0, 1) => (-1, 0),
+        (1, 0) => (0, 1),
+        _ => panic!("invalid facing {:?}", facing),
+    }
+}
+
+pub fn part1() {
+    let mut hull = HashMap::new();
+
+    paint_hull(&mut hull, 0);
 
     println!("Total panels painted: {}", hull.len());
+}
+
+pub fn part2() {
+    let mut hull = HashMap::new();
+
+    paint_hull(&mut hull, 1);
+
+    let mut min_x = i32::MAX;
+    let mut max_x = i32::MIN;
+    let mut min_y = i32::MAX;
+    let mut max_y = i32::MIN;
+
+    for (x, y) in hull.keys() {
+        if *x < min_x {
+            min_x = *x;
+        } else if *x > max_x {
+            max_x = *x;
+        }
+
+        if *y < min_y {
+            min_y = *y;
+        } else if *y > max_y {
+            max_y = *y;
+        }
+    }
+
+    let mut image = Vec::new();
+
+    for _ in min_y..=max_y {
+        image.push(vec![0; (max_x - min_x + 1) as usize]);
+    }
+
+    for ((x, y), color) in hull.iter() {
+        image[*y as usize][*x as usize] = *color;
+    }
+
+    for row in image {
+        println!(
+            "{}",
+            row.iter()
+                .map(|number| if *number == 0 { " " } else { "#" })
+                .collect::<Vec<_>>()
+                .join("")
+        );
+    }
 }
